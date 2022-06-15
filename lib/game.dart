dcfd83224b8406609a2e80230528b5a0c468da64
Ivan Y <ivany@subetuweb.site>
2022-06-14 21:45:53 -0500

    Improving playing c-1001

--- lib/game.dart
+++ lib/game.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'elementModel.dart';
+import 'element.dart';
 
 class Game extends StatefulWidget {
   const Game({Key? key}) : super(key: key);
@@ -8,16 +11,22 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+
   int playerOneScore = 0;
   int playerTwoScore = 1;
+  List<ElementModel> elements=[];
   bool isThinking=false;
   int touches=0;
-  List<bool> _playerOne = [true, false, true, false, true, false, true, false, false];
 
-  void touchedChangePlayer(){
+  void touchedChangePlayer(int i){
     setState((){
+      elements[i] = ElementModel(
+        id: touches,
+        player: touches%2==0 ? 'player_one' : 'player_two',
+        position: i
+      );
       touches++;
-      isThinking=_playerOne[touches];
+      isThinking=!isThinking;
     });
   }
 
@@ -61,27 +70,40 @@ class _GameState extends State<Game> {
                       height: width*0.6,
                       child: GridView.builder(
                         itemCount: 9,
-                        itemBuilder: (BuildContext ctx, i){
-                          return IconSelected(callback: () => touchedChangePlayer(), playerOne: _playerOne[touches]);
-                        },
                         physics: const NeverScrollableScrollPhysics(),
                         padding: const EdgeInsets.only(top: 5.0, left: 5.0),
                         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+                        itemBuilder: (BuildContext ctx, i){
+                          if(elements.isEmpty){
+                            elements.insert(i, ElementModel(
+                              position: i,
+                            ));
+                          }
+                          if(elements.length < i+1){
+                            elements.insert(i, ElementModel(
+                              position: i,
+                            ));
+                          }
+                          return GridElement(
+                            callback:() => touchedChangePlayer(i),
+                            elementData: elements[i]
+                          );
+                        },
                       ),
                     ),
                   )
                 ],
               )
             ),
-            Positioned(
+            Positioned( //TODO: notification widget isolated
               bottom: height/8,
-              left: width/2-20,
+              left: width/2-25,
               child: Visibility(
-                visible: isThinking,
-                child: const Text(
-                  'Thinking...', //TODO: animate dots
-                  style: TextStyle(
-                      fontSize: 20,
+                visible: true,  ///TODO: when its visible isThinking
+                child: Text(
+                  touches%2==0 ? '' : 'thinking...', //TODO: animate dots
+                  style: const TextStyle(
+                      fontSize: 18,
                       color: Color(0xFFD4D4D4)
                   ),
                 )
@@ -93,43 +115,19 @@ class _GameState extends State<Game> {
     );
   }
 }
-
-class IconSelected extends StatefulWidget {
-  final Function callback;
-  final bool playerOne;
-  const IconSelected({Key? key, required this.callback, required this.playerOne}) : super(key: key);
-  @override
-  State<IconSelected> createState() => _IconSelected();
+class OnePlayer{
+  /// user vs algorithm
+  /// notification: your turn?
+  /// start random icon
+  /// space model: touched, icon, step, player
+  /// game over when someone wins or no more white spaces
 }
 
-class _IconSelected extends State<IconSelected> {
-  bool _touched=false;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(3.0),
-      child: GestureDetector(
-        onTap: (){
-          setState((){
-            if(!_touched){
-              _touched=true;
-              widget.callback();
-            }
-          });
-        },
-        child: AnimatedOpacity(
-            duration: const Duration(milliseconds: 500),
-            opacity: _touched ? 1.0 : 0.0,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 10.0),
-              child: SvgPicture.asset(
-                widget.playerOne ? 'assets/circle.svg' : 'assets/cross.svg',
-                width: 26,
-                height: 26,
-              ),
-            )
-        ),
-      ),
-    );
-  }
+class TwoPlayers{
+  /// user vs user
+  /// notification: your turn?
+  /// start random icon
+  /// space model: touched, icon, step, player
+  /// who wins: notification player or symbol name - display line gray showing the winner path
+  /// game over when someone wins or no more white spaces
 }
